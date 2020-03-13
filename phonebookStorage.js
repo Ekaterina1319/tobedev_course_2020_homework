e0a494f85189c11ef06fc8e1705a28e38b97f77a
Kate <borisovakate02@gmail.com>
2020-03-13 21:11:05 +0300

    i newCommit

---
+++ phonebookStorage.js
@@ -0,0 +1,3 @@
+let phonebookStorage = [];
+
+module.exports = phonebookStorage;
---
+++ validation.js
@@ -0,0 +1,26 @@
+
+function dataCheking (phone) {
+    var phoneno = /^((\+7|7|8)+([0-9]){10})$/;
+
+    if(phone.match(phoneno)) {
+        return(true); 
+    } else {
+            return(false);
+      }  
+}
+
+
+function nameVerification (name) {
+    //var rus = /((^[А-Я])([а-яё]+))\s(([А-Я])([а-яё]+))/g; 
+      var rus = /((^[А-Я])([а-яё]+))/g;
+    
+        if (name.match(rus)) {
+             return(true);
+        } else {
+            return(false);
+          }
+}
+
+
+module.exports.dataCheking = dataCheking;
+module.exports.nameVerification = nameVerification;
